d84172b96656a615370841ad97849b4044ccf112
Charlie the Unicorn <charlie@charles-edouard.lecat-HP-EliteBook-840-G3>
2017-02-02 20:00:32 +0100

    Test

--- src/components/Component.hpp
+++ src/components/Component.hpp
@@ -4,6 +4,7 @@
 # include "Input.hpp"
 # include "True.hpp"
 # include "False.hpp"
+# include "Clock.hpp"
 
 # include "c4001.hpp"
 # include "c4008.hpp"
@@ -11,6 +12,7 @@
 # include "c4013.hpp"
 # include "c4017.hpp"
 # include "c4030.hpp"
+# include "c4040.hpp"
 # include "c4069.hpp"
 # include "c4071.hpp"
 # include "c4081.hpp"
